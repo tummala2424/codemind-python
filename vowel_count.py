77792eaaa11ab5c2947ee29b8fae26c6ac9f9066
Devika <97424635+tummala2424@users.noreply.github.com>
2022-07-02 19:01:02 +0530

    n=input() count=0 for char in n:     if char=='a' or char=='e' or char=='i' or char=='o' or char=='u'or char=='A' or char=='E' or char=='I' or char=='O' or char=='U':         count=count+1 print(count)

---
+++ vowel_count.py
@@ -0,0 +1,6 @@
+n=input()
+count=0
+for char in n:
+    if char=='a' or char=='e' or char=='i' or char=='o' or char=='u'or char=='A' or char=='E' or char=='I' or char=='O' or char=='U':
+        count=count+1
+print(count)
