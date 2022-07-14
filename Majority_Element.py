6415d94539798b8e293108aafcc037a82eba83a1
Devika <97424635+tummala2424@users.noreply.github.com>
2022-07-14 22:53:41 +0530

    n=int(input()) arr=list(map(int,input().split())) m=0 for i in range(len(arr)):     count=0     for j in range(len(arr)):         if arr[i]==arr[j]:             count=count+1     if count>m:         m=count         k=arr[i] print(k)

---
+++ Majority_Element.py
@@ -0,0 +1,12 @@
+n=int(input())
+arr=list(map(int,input().split()))
+m=0
+for i in range(len(arr)):
+    count=0
+    for j in range(len(arr)):
+        if arr[i]==arr[j]:
+            count=count+1
+    if count>m:
+        m=count
+        k=arr[i]
+print(k)
