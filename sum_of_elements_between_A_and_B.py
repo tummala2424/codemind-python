83243bfde3934aaf914efd734de0e97a3cf875bc
Devika <97424635+tummala2424@users.noreply.github.com>
2022-07-17 15:04:48 +0530

    n=int(input()) a=list(map(int,input().split())) b,c=map(int,input().split()) k=0 min=0 for i in range(n):     if a[i]>=b and a[i]<=c:         k=1         min=min+a[i] if k==0:     print(-1) else:     print(min)

---
+++ sum_of_elements_between_A_and_B.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+b,c=map(int,input().split())
+k=0
+min=0
+for i in range(n):
+    if a[i]>=b and a[i]<=c:
+        k=1
+        min=min+a[i]
+if k==0:
+    print(-1)
+else:
+    print(min)
