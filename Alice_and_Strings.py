5699f842bbc115fd539b31eb9d5d583175a7c171
Devika <97424635+tummala2424@users.noreply.github.com>
2022-08-04 10:45:21 +0530

    n=int(input()) for i in range(n):     s=input()     s1=input()     c=0     for i in range(len(s)):         if ord(s[i])<=ord(s1[i]):             c+=1     if c==len(s):         print("YES")     else:         print("NO")

---
+++ Alice_and_Strings.py
@@ -0,0 +1,12 @@
+n=int(input())
+for i in range(n):
+    s=input()
+    s1=input()
+    c=0
+    for i in range(len(s)):
+        if ord(s[i])<=ord(s1[i]):
+            c+=1
+    if c==len(s):
+        print("YES")
+    else:
+        print("NO")
